f87afff05ee079133458803281cdce94750fd266
Muhammad Moaaz bin Sajjad <moaaz88sajjad@gmail.com>
2022-08-27 20:15:41 +0500

    Added ghoul animations and audio

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,24 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-public class EnemyController : MonoBehaviour
-{
-    [SerializeField] NavMeshAgent EnemyAgent;
-    [SerializeField] Transform EnemyCharacter;
-    [SerializeField] float DistanceFromPlayer;
-    [SerializeField] float MinimumDistanceLimit;
-    [SerializeField] bool isDetected;
-    [SerializeField] ParticleSystem SpawnParticles;
 
+public class EnemyController: MonoBehaviour
+{
+    [SerializeField] Transform EnemyCharacter;
+    [SerializeField] float MinimumDistanceLimit;
+    [SerializeField] float AlarmDistance;
+    [SerializeField] ParticleSystem SpawnParticles;
 
     [Header("Runtime Game Hierarchy References")]
 
     Transform[] EnemyChildTransforms;
     GameObject Player;
     Animator EnemyAnimator;
+    AudioSource alarm;
+    AudioSource scream;
+    NavMeshAgent EnemyAgent;
+    float DistanceFromPlayer;
+    bool isDead = false; // Initially, the enemy is alive
+    bool isDetected = false; // Initially, the enemy has not been detected
 
+    [Header("Hash Conversions for Optimisation")]
 
+    readonly int isRunninghash = Animator.StringToHash("isRunning");
+    readonly int isDeadhash = Animator.StringToHash("isDead");
 
     // Start is called before the first frame update
     void Start()
@@ -27,55 +32,68 @@ public class EnemyController : MonoBehaviour
         Player = GameObject.FindGameObjectWithTag("Player");
         EnemyAnimator = EnemyCharacter.gameObject.GetComponent<Animator>();
         EnemyChildTransforms = EnemyCharacter.transform.GetComponentsInChildren<Transform>();
+        alarm = GetComponent<AudioSource>();
+        scream = Player.GetComponent<AudioSource>();
 
         UpdateLayerMask("Hidden");
-
-
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        DistanceCalculation();
-
+        if (isDead) // Do nothing if enemy has died.
+            return;
         if (isDetected)
-        {
             Chase();
-        }
+        else
+            DistanceCalculation();
     }
 
     private void DistanceCalculation() 
     {
         DistanceFromPlayer = (Player.transform.position - transform.position).magnitude;
-        
-        if (DistanceFromPlayer < MinimumDistanceLimit && !isDetected)
+        if (DistanceFromPlayer < AlarmDistance && !alarm.isPlaying)
+            alarm.Play();
+        if (DistanceFromPlayer < MinimumDistanceLimit)
         {
             isDetected = true;
             UpdateLayerMask("Default");
-
             Instantiate(SpawnParticles,transform.position,Quaternion.identity);
+            scream.Play();
+            EnemyAnimator.SetBool(isRunninghash, true);
         }
     }
 
     private void Chase() 
     {
         EnemyAgent.SetDestination(Player.transform.position);
-
         transform.rotation = Quaternion.LookRotation(Player.transform.position - transform.position);
     }
+
     private void UpdateLayerMask(string layerName) 
     {
-        int LayerNum = 0;
-
-        LayerNum = LayerMask.NameToLayer(layerName);
+        int LayerNum = LayerMask.NameToLayer(layerName);
 
         for (int i = 0; i < EnemyChildTransforms.Length; i++)
         {
             EnemyChildTransforms[i].gameObject.layer = LayerNum;
         }
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Trap"))
+        {
+            EnemyAnimator.SetBool(isDeadhash, true);
+            isDead = true;
+            
+            // to stop the ghost movement once it dies
+            EnemyAgent.velocity = Vector3.zero;
+            EnemyAgent.angularSpeed = 0;
+            GetComponent<Rigidbody>().freezeRotation = true;
 
+            // disable the collider so that Courage does not kick around the dead ghost
+            GetComponent<CapsuleCollider>().enabled = false;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController: MonoBehaviour
 {
     Vector2 playerDirection;
     Animator playerAnimator;
-
     [SerializeField] Joystick Joystick;
-    
     [SerializeField] Transform PlayerCharacter;
-
     [SerializeField] float PlayerSpeed;
-
     [SerializeField] Rigidbody PlayerRigidbody;
 
     // Start is called before the first frame update
@@ -26,32 +20,24 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Player movement controls system
-        
         float xMov = Joystick.Horizontal;
         float zMov = Joystick.Vertical;
 
        // transform.Translate( xMov * Time.deltaTime * PlayerSpeed , 0 , zMov * Time.deltaTime * PlayerSpeed );
-
         var vectorsample = new Vector3(xMov * Time.deltaTime * PlayerSpeed, 0, zMov * Time.deltaTime * PlayerSpeed) * 100;
-
         PlayerRigidbody.velocity = vectorsample;
 
         //Player rotation control system
-       
         if (zMov != 0 && xMov != 0)
         {
             playerDirection.x = zMov;
             playerDirection.y = xMov;
         }
-
-        
         Vector2 direction = playerDirection - Vector2.zero;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        
         PlayerCharacter.rotation = Quaternion.Euler(0, angle, 0);
 
         //Player Animation Control System
-
         if (zMov != 0 || xMov != 0)
         {
             playerAnimator.SetBool("isRunning",true);
